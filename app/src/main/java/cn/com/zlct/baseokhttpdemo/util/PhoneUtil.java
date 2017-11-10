1c58ad2ee69b05f73fde3436d3ee52667f8b2ea7
SamSongShan <1135520344@qq.com>
2017-11-10 17:57:44 +0800

    Update PhoneUtil.java

--- app/src/main/java/cn/com/zlct/baseokhttpdemo/util/PhoneUtil.java
+++ app/src/main/java/cn/com/zlct/baseokhttpdemo/util/PhoneUtil.java
@@ -33,7 +33,24 @@ import cn.com.zlct.baseokhttpdemo.custom.CircleTextView;
  * 获取手机相关信息
  */
 public class PhoneUtil {
-
+ /**
+     * 获取当前应用程序的包名
+     * @param context 上下文对象
+     * @return 返回包名
+     */
+    public static String getAppProcessName(Context context) {
+        //当前应用pid
+        int pid = android.os.Process.myPid();
+        //任务管理类
+        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        //遍历所有应用
+        List<ActivityManager.RunningAppProcessInfo> infos = manager.getRunningAppProcesses();
+        for (ActivityManager.RunningAppProcessInfo info : infos) {
+            if (info.pid == pid)//得到当前应用
+                return info.processName;//返回包名
+        }
+        return "";
+    }
     /**
      * 获取手机屏幕宽度
      */
